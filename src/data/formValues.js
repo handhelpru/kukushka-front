2dad824caa05a6eb25bad5178bd40bdd1e0bd02d
todatasudata <vitaly.esipov@gmail.com>
2022-02-13 12:26:49 +0300

    update interface

---
+++ src/data/formValues.js
@@ -0,0 +1,22 @@
+const drugsList = [
+  { name: "Каннабис" },
+  { name: "Гашиш" },
+  { name: "Мефедрон" },
+  { name: "Амфетамин" },
+  { name: "МДМА" },
+  { name: "Спайс" },
+  { name: "Соль" },
+  { name: "Героин" },
+  { name: "Метадон" },
+  { name: "Дезоморфин" },
+];
+
+const sexList = [{ name: "Мужской" }, { name: "Женский" }];
+
+const regionsList = [
+  { name: "Санкт-Петербург" },
+  { name: "Краснодарский край" },
+  { name: "Новосибирск" },
+];
+
+export { drugsList, sexList, regionsList };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,60 +3,122 @@
   <div class="grid flex justify-content-center">
     <div class="col-6 md:col-6 p-fluid flex justify-content-center">
       <div class="card">
-        <h2>В чем обвиняют?</h2>
-        <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
-          </div>
-        </div>
         <h2>Какой наркотик?</h2>
         <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
+          <div class="col-12 mb-2 lg:mb-0">
+            <Dropdown v-model="drug" :options="drugsList" optionLabel="name" />
           </div>
         </div>
         <h2>Количество (в граммах)</h2>
         <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
+          <div class="col-12 mb-2 lg:mb-0">
+            <InputNumber v-model="amount" mode="decimal" suffix=" г" />
           </div>
         </div>
         <h2>Пол</h2>
         <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
+          <div class="col-12 mb-2 lg:mb-0">
+            <Dropdown v-model="sex" :options="sexList" optionLabel="name" />
           </div>
         </div>
         <h2>Регион (Москвы пока нет)</h2>
         <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
+          <div class="col-12 mb-2 lg:mb-0">
+            <Dropdown
+              v-model="region"
+              :options="regionsList"
+              optionLabel="name"
+            />
           </div>
         </div>
         <h2>Есть ли судимость?</h2>
         <div class="grid formgrid">
-          <div class="col-12 mb-2 lg:col-4 lg:mb-0">
-            <Dropdown />
+          <div class="col-12 mb-2 lg:mb-0">
+            <Dropdown
+              v-model="conviction"
+              :options="convictionList"
+              optionLabel="name"
+            />
+          </div>
+        </div>
+        <div v-if="conviction.value">
+          <h2>Отбывали наказание?</h2>
+          <div class="grid formgrid">
+            <div class="col-12 mb-2 lg:mb-0">
+              <Dropdown
+                v-model="imprisonment"
+                :options="imprisonmentList"
+                optionLabel="name"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
   <div class="grid flex justify-content-center m-5">
-    <div class="col-4 md:col-4 p-fluid flex justify-content-center">
-      <Button label="Узнать прогноз" class="p-button-lg" />
+    <div class="p-fluid flex justify-content-center">
+      <Button
+        label="Узнать прогноз"
+        class="p-button-lg"
+        @click="getMetaData()"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Button from "primevue/button";
 import Dropdown from "primevue/dropdown";
+import InputNumber from "primevue/inputnumber";
+import { drugsList, sexList, regionsList } from "@/data/formValues";
 
 export default {
   components: {
     Dropdown,
     Button,
+    InputNumber,
+  },
+  data() {
+    return {
+      metadata: null,
+      drug: String,
+      amount: 0,
+      sex: String,
+      region: String,
+      conviction: Boolean,
+      imprisonment: Boolean,
+      drugsList: [],
+      sexList: [],
+      regionsList: [],
+      convictionList: [
+        { name: "Да", value: true },
+        { name: "Нет", value: false },
+      ],
+      imprisonmentList: [
+        { name: "Да", value: true },
+        { name: "Нет", value: false },
+      ],
+      predict: {},
+    };
+  },
+  methods: {
+    async getMetaData() {
+      const url = "http://65.108.56.20:40000/metadata";
+      const response = await axios.get(url);
+      console.log(response);
+    },
+    async getPredict() {
+      const url = "http://65.108.56.20:40000/predict";
+      // const payload = {};
+      await axios.post(url);
+    },
+  },
+  mounted() {
+    this.drugsList = drugsList;
+    this.sexList = sexList;
+    this.regionsList = regionsList;
   },
 };
 </script>
