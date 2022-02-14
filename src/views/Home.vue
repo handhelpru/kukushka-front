7e6b638f4df033ba0925cd7cde45f53b7fb8c56f
todatasudata <vitaly.esipov@gmail.com>
2022-02-14 11:21:43 +0300

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -110,6 +110,7 @@ export default {
   methods: {
     async getPredict() {
       const url = `${process.env.VUE_APP_API_HOST}/predict`;
+      console.log(url);
       const payload = {
         sex: this.sex.name,
         region: this.region.name,
